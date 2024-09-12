1953b817d0a3c7bedf664f7356d31a02e6cb8a69
under-a <mymail15@i.ua>
2024-09-12 19:45:16 +0300

    Solution

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -2,6 +2,17 @@ type GroupsMap<T> = {
   [key: string]: T[];
 };
 
-export function groupByKey(items, key) {
-  // write code here;
+export function groupByKey<T extends { [key: string]: number | string }>(
+  items: T[],
+  key: string,
+): GroupsMap<T> {
+  return items.reduce((result: GroupsMap<T>, item: T) => {
+    if (!result[item[key]]) {
+      result[item[key]] = [];
+    }
+
+    result[item[key]].push(item);
+
+    return result;
+  }, {});
 }
