088d70acb51e4f4490ec2ce9361251116edf4a72
under-a <mymail15@i.ua>
2024-09-12 20:07:13 +0300

    Added typeguard

--- src/groupByKey.ts
+++ src/groupByKey.ts
@@ -7,11 +7,16 @@ export function groupByKey<T extends { [key: string]: number | string }>(
   key: string,
 ): GroupsMap<T> {
   return items.reduce((result: GroupsMap<T>, item: T) => {
-    if (!result[item[key]]) {
-      result[item[key]] = [];
-    }
+    if (
+      item[key] &&
+      (typeof item[key] === 'string' || typeof item[key] === 'number')
+    ) {
+      if (!result[item[key]]) {
+        result[item[key]] = [];
+      }
 
-    result[item[key]].push(item);
+      result[item[key]].push(item);
+    }
 
     return result;
   }, {});
